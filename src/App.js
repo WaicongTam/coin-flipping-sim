bfcfd953ad522939d92337ca43c47ce9e08e919e
Weicong Tan <wtan0047@student.monash.edu>
2021-03-19 10:33:37 +1100

    Set the several panels in the App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className="App-CoinPanel">
+
+      </div>
+
+      <div className="App-ControlPanel">
+
+      </div>
+
+      <div className="App-HistoryPanel">
+
+      </div>
+
+      <div className="App-PlotPanel">
+
+      </div>
       
     </div>
   );
