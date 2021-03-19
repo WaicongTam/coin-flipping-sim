af4d8a782cc81023540e977b676196b0c62d4165
Weicong Tan <wtan0047@student.monash.edu>
2021-03-19 15:22:02 +1100

    Start writing CoinPanel.js

---
+++ src/CoinPanel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Coin from './Coin';
+
+class CoinPanel extends Component {
+
+    render() {
+
+        return (
+
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CoinPanel;
