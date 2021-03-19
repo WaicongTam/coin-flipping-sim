b23405ca3083ad2d34d6b8c7292fc2111ca41801
Weicong Tan <wtan0047@student.monash.edu>
2021-03-19 21:55:13 +1100

    Added some default state
    So, we changed the way to pass the props to the sub-component, which makes it easier to test and reuse.

--- src/CoinFlippingSim.js
+++ src/CoinFlippingSim.js
@@ -4,15 +4,26 @@ import CoinPanel from './CoinPanel';
 
 class CoinFlippingSim extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            alpha: 2,
+            beta: 2,
+            face: 'head',
+            realProb: 0.5,
+        }
+    }
+
     render() {
         return (
             <div className="CoinFlippingSim">
                 <div className="CoinFlippingSim-CoinPanel">
-                    <CoinPanel face="head"/>
+                    <CoinPanel face={ this.state.face }/>
                 </div>
     
                 <div className="CoinFlippingSim-PlottingPanel">
-                    <PlottingPanel alpha="2" beta="2" />
+                    <PlottingPanel alpha={ this.state.alpha } beta={ this.state.beta } />
                 </div>
             </div>
         );
