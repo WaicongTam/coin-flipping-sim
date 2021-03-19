63329d2a31659a8dc85f0cfa11bd2212aa185586
Weicong Tan <wtan0047@student.monash.edu>
2021-03-19 15:21:12 +1100

    Import the image element of the coin

--- src/Coin.js
+++ src/Coin.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
 class Coin extends Component {
 
     render() {
 
+        let face = this.props.face;
+
         return (
 
             <div>
-
+                <i class={`fas fa-circle ${face}`}></i>
             </div>
         );
     }
