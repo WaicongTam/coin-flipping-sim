80f56e02512f4e1dcc9edb3fb5f44d955053beda
Weicong Tan <wtan0047@student.monash.edu>
2021-03-23 15:28:33 +1100

    Import the minimum css of bootstrap

--- src/CoinFlippingSim.js
+++ src/CoinFlippingSim.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import PlottingPanel from './PlottingPanel';
 import CoinPanel from './CoinPanel';
 import InputPanel from './InputPanel';
