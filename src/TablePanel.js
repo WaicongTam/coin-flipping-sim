2ce352f0f46eabed48adbace60a8529df8817f33
Weicong Tan <wtan0047@student.monash.edu>
2021-03-23 15:32:31 +1100

    Unwrap the table from the div

--- src/TablePanel.js
+++ src/TablePanel.js
@@ -6,30 +6,28 @@ class TablePanel extends Component {
 
     render() {
         return (
-            <div>
-                <Table>
-                    <tbody>
-                        <th className="Table-Header"># of flipping</th>
-                        <th className="Table-Header">Result</th>
-                        <th className="Table-Header">Sequence</th>
-                        <th className="Table-Header">Knowledge</th>
-                        { this.props.data.map(r => <tr className="Table-Record">
-                            <td>
-                                { r.number }
-                            </td>
-                            <td>
-                                { r.result }
-                            </td>
-                            <td>
-                                { r.sequence.length > 0 ? `[${r.sequence.join(', ')}]` : '-' }
-                            </td>
-                            <td>
-                                { r.knowledge }
-                            </td>
-                        </tr>) }
-                    </tbody>
-                </Table>
-            </div>
+            <Table>
+                <tbody>
+                    <th className="Table-Header"># of flipping</th>
+                    <th className="Table-Header">Result</th>
+                    <th className="Table-Header">Sequence</th>
+                    <th className="Table-Header">Knowledge</th>
+                    { this.props.data.map(r => <tr className="Table-Record">
+                        <td>
+                            { r.number }
+                        </td>
+                        <td>
+                            { r.result }
+                        </td>
+                        <td>
+                            { r.sequence.length > 0 ? `[${r.sequence.join(', ')}]` : '-' }
+                        </td>
+                        <td>
+                            { r.knowledge }
+                        </td>
+                    </tr>) }
+                </tbody>
+            </Table>
         );
     }
 }
